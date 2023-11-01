f9582f4d87f8fc85aa201768364bb670d76ab7f2
cakleffman <cakleffman@gmail.com>
2023-11-01 15:24:27 -0600

    First Draft of DB WIP

---
+++ lib/database/LocationManager.dart
@@ -0,0 +1,119 @@
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'Location.dart';
+
+
+
+class LocationManager {
+  late Database database;
+
+  LocationManager() {
+    _initializeDatabase();
+  }
+
+  Future<void> _initializeDatabase() async {
+    String databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'WhereHouse.db');
+
+    database = await openDatabase(
+      path,
+      onCreate: (db, version) async {
+        await db.execute(
+          'CREATE TABLE IF NOT EXISTS Location('
+              'uid INTEGER PRIMARY KEY, '
+              'name TEXT, '
+              'defaultLocation INTEGER'
+              ')',
+        );
+      },
+      version: 1,
+    );
+  }
+
+  // Add location method
+  Future<bool> addLocation(
+      int uid,
+      String name,
+      int defaultLocation,
+      ) async {
+    try {
+      final result = await database.query(
+        'Location',
+        where: 'uid = ?',
+        whereArgs: [uid],
+      );
+
+      if (result.isNotEmpty) {
+        return false;
+      }
+
+      Location newLocation = Location(
+        uid: uid,
+        name: name,
+        defaultLocation: defaultLocation,
+      );
+
+      bool success = await newLocation.setLocation();
+      return success;
+    } catch (e) {
+      print('Error adding location: $e');
+      return false;
+    }
+  }
+
+  // Remove location method
+  Future<bool> removeLocation(int uid) async {
+    try {
+      int rowsDeleted = await database.delete('Location', where: 'UID = ?', whereArgs: [uid]);
+      return rowsDeleted > 0;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  // Edit location method
+  Future<Location?> editLocation({
+    required int uid,
+    String? name,
+    int? defaultLocation,
+  }) async {
+    Location existingLocation = await Location.getLocation(uid);
+
+    if (existingLocation.uid == uid) {
+      if (name != null) existingLocation.name = name;
+      if (defaultLocation != null) existingLocation.defaultLocation = defaultLocation;
+
+      try {
+        await existingLocation.setLocation();
+        return existingLocation;
+      } catch (e) {
+        print(e);
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  // Query locations method
+  Future<List<Location>> queryLocations(String query) async {
+    try {
+      List<Map> results = await database.query('Location',
+          where: 'name LIKE ? OR UID LIKE ?',
+          whereArgs: List.filled(2, '%$query%'));
+      return results.map((location) {
+        return Location(
+          uid: location['uid'],
+          name: location['name'],
+          defaultLocation: location['defaultLocation'],
+        );
+      }).toList();
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
+}
---
+++ lib/database/UserManager.dart
@@ -0,0 +1,119 @@
+import 'dart:async';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'User.dart';
+
+class UserManager {
+  late Database database;
+
+  UserManager() {
+    _initializeDatabase();
+  }
+
+  Future<void> _initializeDatabase() async {
+    String databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'WhereHouse.db');
+
+    database = await openDatabase(
+      path,
+      onCreate: (db, version) async {
+        await db.execute(
+          'CREATE TABLE IF NOT EXISTS User('
+              'uid INTEGER PRIMARY KEY, '
+              'name TEXT, '
+              'checkedOutItems TEXT'
+              ')',
+        );
+      },
+      version: 1,
+    );
+  }
+
+  Future<bool> addUser(
+      int uid,
+      String name,
+      Map<int, int> checkedOutItems,
+      ) async {
+    try {
+      final result = await database.query(
+        'User',
+        where: 'uid = ?',
+        whereArgs: [uid],
+      );
+
+      if (result.isNotEmpty) {
+        return false;
+      }
+
+      User newUser = User(
+        uid: uid,
+        name: name,
+        checkedOutItems: checkedOutItems,
+      );
+
+      bool success = await newUser.setUser();
+
+      return success;
+    } catch (e) {
+      print('Error adding user: $e');
+      return false;
+    }
+  }
+
+  Future<bool> removeUser(int uid) async {
+    try {
+      int rowsDeleted = await database.delete('User', where: 'uid = ?', whereArgs: [uid]);
+      return rowsDeleted > 0;
+    } catch (e) {
+      print(e);
+      return false;
+    }
+  }
+
+  Future<User?> editUser({
+    required int uid,
+    String? name,
+    Map<int, int>? checkedOutItems,
+  }) async {
+    User? existingUser;
+
+    try {
+      existingUser = await User.getUser(uid);
+
+      if (existingUser.uid == uid) {
+        if (name != null) existingUser.name = name;
+        if (checkedOutItems != null) existingUser.checkedOutItems = checkedOutItems;
+
+        await existingUser.setUser();
+        return existingUser;
+      } else {
+        return null;
+      }
+    } catch (e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future<List<User>> queryUsers(String query) async {
+    try {
+      List<Map> results = await database.query(
+        'User',
+        where: 'name LIKE ? OR UID LIKE ?',
+        whereArgs: List.filled(2, '%$query%'),
+      );
+
+      return results.map((user) {
+        return User(
+          uid: user['uid'],
+          name: user['name'],
+          checkedOutItems: Map<int, int>.from(user['checkedOutItems']),
+        );
+      }).toList();
+    } catch (e) {
+      print(e);
+      return [];
+    }
+  }
+}
