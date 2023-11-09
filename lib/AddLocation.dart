c49cc2443c3f24bc022d2efe705554026a22d7ef
ASIF RAHMAN <53287563+ucchol@users.noreply.github.com>
2023-11-09 01:18:52 -0700

    AddLocation.dart

--- lib/AddLocation.dart
+++ lib/AddLocation.dart
@@ -1,61 +1,65 @@
-import 'Location.dart';
-import 'LocationView.dart';
-import 'LocationListView.dart';
 import 'package:flutter/material.dart';
+import 'Location.dart';
+import 'LocationManager.dart'; 
 
-class LocationView extends StatelessWidget {
-  final Location location;
+void main() => runApp(MaterialApp(home: LocationController()));
 
-  LocationView(this.location);
-
+// LocationController widget to interface with the LocationManager.
+class LocationController extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Location View'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Location Name: ${location.name}'),
-            Text('Description: ${location.description}'),
-            Text('Barcodes: ${location.barcodes.toString()}'),
-            Text('Locations: ${location.locations.toString()}'),
-          ],
-        ),
-      ),
-    );
-  }
+  _LocationControllerState createState() => _LocationControllerState();
 }
 
-class LocationListView extends StatelessWidget {
-  final List<Location> locationList;
+class _LocationControllerState extends State<LocationController> {
+  final LocationManager locationManager = LocationManager.instance;
 
-  LocationListView(this.locationList);
+  // Method to add a new location to the LocationManager.
+  void addLocationToLocationManager(Location location) {
+    // Precondition checks
+    assert(locationManager != null && locationManager.isOpen(),
+        'LocationManager must not be null and should be open.');
+
+    // Adding a new location to the LocationManager
+    setState(() {
+      locationManager.addLocation(location);
+    });
+
+    // Postcondition check (in debug mode only)
+    assert(locationManager.contains(location),
+        'LocationManager should now contain the new location.');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Location List View'),
+        title: Text('Location Controller'),
       ),
-      body: ListView.builder(
-        itemCount: locationList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            title: Text('Location Name: ${locationList[index].name}'),
-            subtitle: Text('Description: ${locationList[index].description}'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => LocationView(locationList[index]),
-                ),
-              );
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: locationManager.getLocations().length,
+              itemBuilder: (context, index) {
+                var location = locationManager.getLocations()[index];
+                return ListTile(
+                  title: Text(location.name),
+                  subtitle: Text(location.description),
+                );
+              },
+            ),
+          ),
+          // Button to add a new location
+          ElevatedButton(
+            onPressed: () {
+              // Here you would typically have a form or another method
+              // to input the new location data instead of hardcoding it.
+              Location newLocation = Location(name: 'New Park', description: 'A new public, urban park.');
+              addLocationToLocationManager(newLocation);
             },
-          );
-        },
+            child: Text('Add New Location'),
+          ),
+        ],
       ),
     );
   }
