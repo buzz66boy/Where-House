5aa8cbda4cf1713ab782bb4eda88e4be57c0c4ef
Alberto O. Delgado <45192770+aodelgado@users.noreply.github.com>
2023-11-14 00:25:32 -0700

    Update scanner_controller.dart

--- lib/Item_Management/Controllers/scanner_controller.dart
+++ lib/Item_Management/Controllers/scanner_controller.dart
@@ -1,19 +1,179 @@
+import 'dart:async';
+import 'dart:io' show Platform;
+import 'package:barcode_scan2/barcode_scan2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:flutter/services.dart';
 
-class BarcodeScannerWidget {
-  String _barcodeResult = 'No barcode scanned yet';
-  void scanBarcode() async {
+void main() => runApp(const App() as Widget);
+
+class App extends StatefulWidget {
+  const App({Key? key}) : super(key: key);
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  ScanResult? scanResult;
+
+  final _flashOnController = TextEditingController(text: 'Flash on');
+  final _flashOffController = TextEditingController(text: 'Flash off');
+  final _cancelController = TextEditingController(text: 'Cancel');
+
+  var _aspectTolerance = 0.00;
+  var _numberOfCameras = 0;
+  var _selectedCamera = -1;
+  var _useAutoFocus = true;
+  var _autoEnableFlash = false;
+
+  static final _possibleFormats = BarcodeFormat.values.toList()
+    ..removeWhere((e) => e == BarcodeFormat.unknown);
+
+  List<BarcodeFormat> selectedFormats = [..._possibleFormats];
+
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration.zero, () async {
+      _numberOfCameras = await BarcodeScanner.numberOfCameras;
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final scanResult = this.scanResult;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Barcode Scanner'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.camera_alt),
+              tooltip: 'Scan',
+              onPressed: _scan,
+            ),
+          ],
+        ),
+        body: ListView(
+          shrinkWrap: true,
+          children: <Widget>[
+            if (scanResult != null)
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      title: const Text('Result Type'),
+                      subtitle: Text(scanResult.type.toString()),
+                    ),
+                    ListTile(
+                      title: const Text('Raw Content'),
+                      subtitle: Text(scanResult.rawContent),
+                    ),
+                    ListTile(
+                      title: const Text('Format'),
+                      subtitle: Text(scanResult.format.toString()),
+                    ),
+                    ListTile(
+                      title: const Text('Format note'),
+                      subtitle: Text(scanResult.formatNote),
+                    ),
+                  ],
+                ),
+              ),
+            CheckboxListTile(
+              title: const Text('Autofocus'),
+              value: _useAutoFocus,
+              onChanged: (checked) {
+                setState(() {
+                  _useAutoFocus = checked!;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: const Text('Flash'),
+              value: _autoEnableFlash,
+              onChanged: (checked) {
+                setState(() {
+                  _autoEnableFlash = checked!;
+                });
+              },
+            ),
+            const ListTile(
+              title: Text('Barcode formats'),
+              dense: true,
+              enabled: false,
+            ),
+            ListTile(
+              trailing: Checkbox(
+                tristate: true,
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                value: selectedFormats.length == _possibleFormats.length
+                    ? true
+                    : selectedFormats.isEmpty
+                        ? false
+                        : null,
+                onChanged: (checked) {
+                  setState(() {
+                    selectedFormats = [
+                      if (checked ?? false) ..._possibleFormats,
+                    ];
+                  });
+                },
+              ),
+              dense: true,
+              enabled: false,
+              title: const Text('Detect barcode formats'),
+              subtitle: const Text(
+                'Select all fromats',
+              ),
+            ),
+            ..._possibleFormats.map(
+              (format) => CheckboxListTile(
+                value: selectedFormats.contains(format),
+                onChanged: (i) {
+                  setState(
+                    () => selectedFormats.contains(format)
+                        ? selectedFormats.remove(format)
+                        : selectedFormats.add(format),
+                  );
+                },
+                title: Text(format.toString()),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _scan() async {
     try {
-      String barcodeResult = await FlutterBarcodeScanner.scanBarcode(
-        '#ff6666', // background color
-        'Cancel', // cancel button text
-        true, // show flash icon
-        ScanMode.BARCODE, // barcode mode
+      final result = await BarcodeScanner.scan(
+        options: ScanOptions(
+          strings: {
+            'cancel': _cancelController.text,
+            'flash_on': _flashOnController.text,
+            'flash_off': _flashOffController.text,
+          },
+          restrictFormat: selectedFormats,
+          useCamera: _selectedCamera,
+          autoEnableFlash: _autoEnableFlash,
+          android: AndroidOptions(
+            aspectTolerance: _aspectTolerance,
+            useAutoFocus: _useAutoFocus,
+          ),
+        ),
       );
-      _barcodeResult = barcodeResult;
-    } catch (e) {
-      _barcodeResult = 'Error: $e';
+      setState(() => scanResult = result);
+    } on PlatformException catch (e) {
+      setState(() {
+        scanResult = ScanResult(
+          rawContent: e.code == BarcodeScanner.cameraAccessDenied
+              ? 'The user did not grant the camera permission!'
+              : 'Unknown error: $e',
+        );
+      });
     }
   }
 }
