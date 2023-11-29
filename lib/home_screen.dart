7db5db33a18a7d985b9651e780ef2d3a6f78ac52
vic12141983 <60449014+vic12141983@users.noreply.github.com>
2023-11-29 15:30:15 -0700

    Update home_screen.dart
    Added button for managing User

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -11,6 +11,11 @@ class MyApp extends StatelessWidget {
   late ItemManager itemManager;
   late ItemController itemController;
   late ScannerController scannerController;
+  /// Added User 
+  late UserController userController;
+  late UserManager userManager;
+  late User user = User(uid: 1,name: "John doe", checkedOutItems: [1,3]);
+  
   MyApp({super.key}) {
     // ItemManager itemManager = ItemManager();
     // ItemController itemController = ItemController(itemManager: itemManager);
@@ -51,6 +56,18 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
+  // User added 
+  userManager = UserManager();
+  userManager.initializeDatabase().then((value) async{
+    bool tempUser = await userManager.addUser(user.name,user.checkedOutItems);
+      if(tempUser!=null){
+        user = tempUser as User;
+      }
+      userManager.queryUsers('John doe').then((value) => print(value.toString()));
+
+    });
+
+  
   late ItemManager itemManager;
   late ItemController itemController;
   late ScannerController scannerController;
@@ -153,7 +170,17 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
                 child: Text("Search Items")),
             ElevatedButton(onPressed: () {}, child: Text("Manage Locations")),
-            ElevatedButton(onPressed: () {}, child: Text("Manage Users")),
+            
+            ElevatedButton(
+                onPressed: () async{
+                  widget.userController.setUserViewActive(context, 1);
+                  print("made it here to users");
+                }, child: Text("Manage User")),
+             ElevatedButton(onPressed: () async{
+
+            }, child: Text("Manage Users")),
+
+            
             ElevatedButton(
                 onPressed: () {}, child: Text("Transaction History")),
             ElevatedButton(onPressed: () {}, child: Text("Settings")),
